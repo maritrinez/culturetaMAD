76bab29e21697ea05f41c8823007f6dc18cbdc78
martínez <mtnezb@gmail.com>
2019-11-26 13:38:12 +0100

    update month position on scroll

--- webapp/src/scripts/hscroll.js
+++ webapp/src/scripts/hscroll.js
@@ -1,11 +1,13 @@
 // d3 imports
 import {select, selectAll} from "d3-selection";
+import {timeFormat} from "d3-time-format";
 
 // Classes imports
 import { Render } from './renders';
 
 
-
+// Constants
+const formatMonth = timeFormat('%B');
 // Render class
 export class Hscroll {
   constructor () {
@@ -38,28 +40,55 @@ export class Hscroll {
 
 
     window.addEventListener('wheel', e => {
-      if (e.deltaY > 0) c.scrollLeft += 30;
-      else c.scrollLeft -= 30;
+      if (e.deltaY > 0 || e.deltaX > 0) c.scrollLeft += 15;
+      else c.scrollLeft -= 15;
 
       // Update info & axis x position
-      render._infoUpdate(c.scrollLeft)
+      render._infoUpdate(c.scrollLeft);
+      this._monthUpdate(c);
     });
   }
 
 
   // - - - PRIVATE FUNCTIONS - - - //
   _setToday(c) {
-    const endX = this._scales.x(new Date()) - 30;
+    const endX = this._scales.x(new Date()),
+          that = this;
 
     const ticksPerRender = 12;
     requestAnimationFrame(function render() {
       for (let i = 0; i < ticksPerRender; i++) {
-        if (c.scrollLeft < endX) c.scrollLeft += i;
+        if (c.scrollLeft < endX) {
+          c.scrollLeft += i;
+          that._monthUpdate(c);
+        }
       }
       
       if (c.scrollLeft < endX) {
         requestAnimationFrame(render);
       }
     });
+  }
+
+  _monthUpdate(c) {
+    const screenMonth = formatMonth(this._scales.x.invert(c.scrollLeft));
+
+    selectAll('.axis.monthly').selectAll('text')
+      .attr('dx', function(d) {
+        return _isActive(d) ? _newX(this.parentNode, c.scrollLeft) : 5;
+      });
+
+
+    // Private
+    function _isActive(d) {
+      if (formatMonth(d) == screenMonth) return true;
+      else return false;
+    }
+
+    function _newX(parent, scroll) {
+      const regExp = /\(([^)]+)\,/,
+            tx = +regExp.exec(parent.getAttribute('transform'))[1];
+      return scroll - tx;
+    }
   }
 }
